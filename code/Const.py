ce3ca0cc2a72063c807f7e88afd25c1c512ffdd8
Yasmim <yasmimlima685@gmail.com>
2026-03-20 21:44:05 -0300

    Creating asset folder Inserting all assets Creating the menu: backgrund, sound and fonts Creating the const file

---
+++ code/Const.py
@@ -0,0 +1,14 @@
+#C
+COLOR_PURPLE = (54, 50, 64 )
+COLOR_WHITE = (255, 255, 255)
+
+#M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEM GAME 2P - COOPERATIVE',
+               'NEM GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+#W
+WIN_WIDTH = 961
+WIN_HEIGHT = 655
